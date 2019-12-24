5ea01ea4e0777ee53f602ef718452bacd96f58eb
dimicuss <dimicuss@gmail.com>
2019-12-24 23:12:35 +0300

    #developing typeScript test

--- es/getTranspositions.ts
+++ es/getTranspositions.ts
@@ -1,15 +1,17 @@
-function isNot(item: any) {
-	return item !== this;
-}
-
-
 export default function getTranspositions<T>(items: T[], callback: Function, transposition: T[] = []): void {
 	if (items.length === 0) {
 		callback(transposition);
 	} else {
-		for (let j = 0; j < items.length; j++) {
-			const filteredItems: T[] = items.filter(isNot, items[j]);
-			const nextTransposition: T[] = [...transposition, items[j]];
+		for (let i = 0; i < items.length; i++) {
+			const filteredItems: T[] = [];
+			const nextTransposition: T[] = [...transposition, items[i]];
+
+			for (let j = 0; j < items.length; j++) {
+                if (items[j] !== items[i]) {
+                    filteredItems.push(items[j]);
+                }
+            }
+
 			getTranspositions(filteredItems, callback, nextTransposition);
 		}
 	}
